c58f1e11fd5a8aed9a74dcad4091f6e04273e0a0
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-03-13 08:58:39 -0300

    feat: controller class methods  pre implemented

---
+++ Src/Controller/Controller.cs
@@ -0,0 +1,34 @@
+using System.Numerics;
+using IController;
+using ModelTask;
+
+namespace Controller
+{
+    public class ControllerTasker : IControllerTasker
+    {
+        public TaskModel CreateTask(TaskModel tasker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTasker(BigInteger id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FinishTasker(BigInteger id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TaskModel> GetTasker()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskModel UpdateTasker(TaskModel tasker, BigInteger id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
