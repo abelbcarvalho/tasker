331df3013aca3d623a35c93f7443216d41fd6832
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-03-13 10:40:42 -0300

    feat: create and pre implement service

---
+++ Src/Service/Service.cs
@@ -0,0 +1,34 @@
+using System.Numerics;
+using IService;
+using ModelTask;
+
+namespace Service
+{
+    public class ServiceTasker : IServiceTasker
+    {
+        public TaskModel CreateTask(TaskModel tasker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTasker(BigInteger id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FinishTasker(BigInteger id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TaskModel> GetTasker()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskModel UpdateTasker(TaskModel tasker, BigInteger id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
