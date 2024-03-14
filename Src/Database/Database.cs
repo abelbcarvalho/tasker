2fa91c8dbbbfecbf913e398e33a2b16af59f6d89
Abel BC <160126794+abelbcarvalho@users.noreply.github.com>
2024-03-14 11:34:41 -0300

    feat: problem with null variables (#8)

--- Src/Database/Database.cs
+++ Src/Database/Database.cs
@@ -1,17 +1,14 @@
 using Npgsql;
-using dotenv.net;
-
-DotEnv.Load();
 
 namespace Database
 {
     public static class DatabaseConnectione
     {
-        private static readonly string HOST = Environment.GetEnvironmentVariable("HOST");
-        private static readonly string USER = Environment.GetEnvironmentVariable("USER");
-        private static readonly string PASSWD = Environment.GetEnvironmentVariable("PASSWD");
-        private static readonly int PORT = int.Parse(Environment.GetEnvironmentVariable("PORT"));
-        private static readonly string DBNAME = Environment.GetEnvironmentVariable("DBNAME");
+        private static readonly string HOST = Environment.GetEnvironmentVariable("HOST") ?? "";
+        private static readonly string USER = Environment.GetEnvironmentVariable("USER") ?? "";
+        private static readonly string PASSWD = Environment.GetEnvironmentVariable("PASSWD") ?? "";
+        private static readonly int PORT = int.Parse(Environment.GetEnvironmentVariable("PORT") ?? "5432");
+        private static readonly string DBNAME = Environment.GetEnvironmentVariable("DBNAME") ?? "";
 
         private static readonly NpgsqlConnection connection = new(
             $"Host={HOST};Username={USER};Password={PASSWD};Database={DBNAME};Port={PORT}"
