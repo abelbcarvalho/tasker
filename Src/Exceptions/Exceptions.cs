18fe8548b624cb05b81cc00b87e8293d07d17015
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-03-14 10:15:42 -0300

    feat: priority unknown exception and file formated

--- Src/Exceptions/Exceptions.cs
+++ Src/Exceptions/Exceptions.cs
@@ -3,18 +3,27 @@ namespace Exceptions
     public class StringException : Exception
     {
         public StringException()
-        {}
-        
-        public StringException(string message): base(message)
-        {}
+        { }
+
+        public StringException(string message) : base(message)
+        { }
     }
 
     public class DateTimeException : Exception
     {
         public DateTimeException()
-        {}
+        { }
 
         public DateTimeException(string message) : base(message)
-        {}
+        { }
+    }
+
+    public class PriorityUnknownException : Exception
+    {
+        public PriorityUnknownException()
+        { }
+
+        public PriorityUnknownException(string message) : base(message)
+        { }
     }
 }
