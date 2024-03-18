46798a84a648d96903e28fba9183efa28f9dcc84
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-03-18 13:02:53 -0300

    feat: get enum value as a string switcher

--- Src/Utility/PriorityEnumUtil.cs
+++ Src/Utility/PriorityEnumUtil.cs
@@ -14,5 +14,18 @@ namespace PriorityEnumUtil
 
             return false;
         }
+
+        public static string EnumValueString(EnumPriority priority)
+        {
+            string value = priority switch
+            {
+                EnumPriority.High => "High",
+                EnumPriority.Medium => "Medium",
+                EnumPriority.Low => "Low",
+                _ => "Unknown"
+            };
+
+            return value;
+        }
     }
 }
