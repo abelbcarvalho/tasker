692a17671e98e4b61ed6a29fa8d68b184149424b
Abel BC <160126794+abelbcarvalho@users.noreply.github.com>
2024-03-19 08:45:57 -0300

    feat: delete finish and list all taskers (#12)
    * feat: delete task method inside database
    
    * feat: finish tasker method done
    
    * feat: method to get all taskers done
    
    * fix: the field complete was added

--- Src/Persistence/PersistenceTasker.cs
+++ Src/Persistence/PersistenceTasker.cs
@@ -15,12 +15,13 @@ namespace PersistenceTasker
                 DatabaseConnectione.OpenConnection();
 
                 using var command = DatabaseConnectione.Command(
-                    "insert into Taskers (title, description, priority, startAt, finishAt) values (@title,@description,@priority,@startAt,@finishAt)"
+                    "insert into Taskers (title, description, priority, complete, startAt, finishAt) values (@title,@description,@priority,@complete,@startAt,@finishAt)"
                 );
 
                 command.Parameters.AddWithValue("@title", tasker.Title);
                 command.Parameters.AddWithValue("@description", tasker.Description);
                 command.Parameters.AddWithValue("@priority", PriorityUtil.EnumValueString(tasker.Priority));
+                command.Parameters.AddWithValue("@complete", tasker.Complete);
                 command.Parameters.AddWithValue("@startAt", tasker.StartAt);
                 command.Parameters.AddWithValue("@finishAt", tasker.FinishAt);
 
@@ -43,6 +44,14 @@ namespace PersistenceTasker
             try
             {
                 DatabaseConnectione.OpenConnection();
+
+                using var command = DatabaseConnectione.Command(
+                    "delete from Taskers where id=@id"
+                );
+
+                command.Parameters.AddWithValue("@id", id);
+
+                command.ExecuteNonQuery();
             }
             catch (Exception e)
             {
@@ -59,6 +68,14 @@ namespace PersistenceTasker
             try
             {
                 DatabaseConnectione.OpenConnection();
+
+                using var command = DatabaseConnectione.Command(
+                    "update Taskers set complete=@complete"
+                );
+
+                command.Parameters.AddWithValue("@complete", true);
+
+                command.ExecuteNonQuery();
             }
             catch (Exception e)
             {
@@ -72,9 +89,32 @@ namespace PersistenceTasker
 
         public List<TaskModel> GetTasker()
         {
+            List<TaskModel> taskers = new();
             try
             {
                 DatabaseConnectione.OpenConnection();
+
+                using var command = DatabaseConnectione.Command(
+                    "select * from Taskers"
+                );
+
+                using var resultSet = command.ExecuteReader();
+
+                while (resultSet.Read())
+                {
+                    TaskModel model = new()
+                    {
+                        Id = resultSet.GetInt32(0),
+                        Title = resultSet.GetString(1),
+                        Description = resultSet.GetString(2),
+                        Priority = PriorityUtil.EnumFromString(resultSet.GetString(3)),
+                        Complete = resultSet.GetBoolean(4),
+                        StartAt = resultSet.GetDateTime(5),
+                        FinishAt = resultSet.GetDateTime(6)
+                    };
+
+                    taskers.Add(model);
+                }
             }
             catch (Exception e)
             {
@@ -85,7 +125,7 @@ namespace PersistenceTasker
                 DatabaseConnectione.CloseConnection();
             }
 
-            return [];
+            return taskers;
         }
 
         public TaskModel UpdateTasker(TaskModel tasker, BigInteger id)
